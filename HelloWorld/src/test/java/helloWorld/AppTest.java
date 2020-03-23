312bdc9e817196a49906324dc9ed7f21630aa2d5
jrlmc-iscteiul <jrlmc@iscte-iul.pt>
2020-03-23 14:25:29 +0000

    Pom.xml atualizado e funcional

---
+++ HelloWorld/src/test/java/helloWorld/AppTest.java
@@ -0,0 +1,32 @@
+package helloWorld;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	private App h;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		h = new App();
+	}
+
+	@Test
+	void test() {
+		assertEquals(h.getMessage(),"Hello World!");
+	}
+
+}
